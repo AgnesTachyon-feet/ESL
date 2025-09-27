648fd060ebc69283cc8f80ca745cb9224cb25e60
4RUN555 <68030316@kmitl.ac.th>
2025-09-27 10:34:36 +0700

    very good runได้

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 `py -m venv venv`
 `venv\Scripts\activate.bat`
 `pip install -r requirements.txt`
+
+YOLOv8 project
--- RealTime_yolov8.py
+++ RealTime_yolov8.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 from ultralytics.models.yolo.detect import DetectionPredictor # ->  from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 import cv2
 
-model = YOLO("mouse-computer.pt")
+model = YOLO("best.pt")
 
 result = model.predict(source="0", show=True) # accepts all formats - img/floder/vid.* ()
 print(result)
